fb0def25ef137056910ac79ca744745a64e2d853
csillagbarnabas <47949207+csillagbarnabas@users.noreply.github.com>
2019-03-23 19:15:18 +0100

    Update vector2.h

--- homework4/vector2.h
+++ homework4/vector2.h
@@ -13,6 +13,10 @@ struct Vector2{
         x = x*v; y = y*v;
     return *this;
     }
+    Vector2<T>& operator/=( T const& v ){
+        x = x/v; y = y/v;
+    return *this;
+    }
 };
 template<typename T>
 Vector2<T> operator+( Vector2<T> const& a, Vector2<T> const& b ){
@@ -20,7 +24,7 @@ Vector2<T> operator+( Vector2<T> const& a, Vector2<T> const& b ){
 }
 template<typename T>
 Vector2<T> operator*( Vector2<T> const& a, T const& b ){
-    return Vector2<T>{ b*a.x, b*a.y };
+    return Vector2<T>{ a.x *b, a.y*b };
 }
 template<typename T>
 Vector2<T> operator*( T const& b, Vector2<T> const& a){
@@ -33,4 +37,17 @@ Vector2<T> operator/( Vector2<T> const& a, T const& b ){
 template<typename T>
 Vector2<T> operator-( Vector2<T> const& a, Vector2<T> const& b ){
     return Vector2<T>{ a.x - b.x, a.y - b.y };
+}
+template<typename T>
+std::ostream& operator<<( std::ostream& o, Vector2<T> const& v )
+{
+o << v.x << " " << v.y;
+return o;
+}
+template<typename T>
+std::istream& operator>>( std::istream& i, Vector2<T> const& v )
+{
+i >> v.x;
+i >> v.y;
+return i;
 }
